543102b6b05b00ff79df4375670eacc36610b3d7
shockpast <shockpast@gmail.com>
2024-05-16 22:23:03 +0300

    chore: removed useless variables and functions

--- source/funcs.cpp
+++ source/funcs.cpp
@@ -6,10 +6,6 @@
 
 namespace funcs
 {
-	static const char metaname[] = "sourcesdk";
-	static int32_t metatype = GarrysMod::Lua::Type::NONE;
-	static const char tablename[] = "hcons_sourcesdk";
-
 	LUA_FUNCTION_STATIC(get_model_cache)
 	{
 		LUA->PushNumber(global::stringtable_server->FindTable("modelprecache")->GetNumStrings());
@@ -22,11 +18,6 @@ namespace funcs
 		LUA->PushCFunction(get_model_cache);
 		LUA->SetField(-2, "get_model_cache");
 		LUA->SetField(-2, "sourcesdk");
-
-		LUA->CreateTable();
-		LUA->PushString("nothing yet here");
-		LUA->SetField(-2, "_");
-		LUA->SetField(-2, "network");
 	}
 
 	void Deinitialize(GarrysMod::Lua::ILuaBase* LUA)
