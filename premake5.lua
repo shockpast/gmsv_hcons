d96f1fe70897f12a5621afc17cb7cfd6e4c818c1
shockpast <shockpast@gmail.com>
2024-05-19 12:09:18 +0300

    chore: project file

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,3 @@
--- Defines which version of the project generator to use, by default
--- (can be overriden by the environment variable PROJECT_GENERATOR_VERSION)
 PROJECT_GENERATOR_VERSION = 3
 
 newoption({
@@ -12,17 +10,13 @@ local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
     "you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
 include(gmcommon)
 
-CreateWorkspace({name = "template", abi_compatible = false})
-    -- Serverside module (gmsv prefix)
-    -- Can define "source_path", where the source files are located
-    -- Can define "manual_files", which allows you to manually add files to the project,
-    -- instead of automatically including them from the "source_path"
-    -- Can also define "abi_compatible", for project specific compatibility
-    CreateProject({serverside = true, manual_files = false})
-        -- Remove some or all of these includes if they're not needed
-        --IncludeHelpersExtended()
+CreateWorkspace({ name = "hcons", abi_compatible = false })
+    CreateProject({ serverside = true, manual_files = false })
         --IncludeLuaShared()
+        IncludeHelpersExtended()
         IncludeSDKCommon()
+        IncludeDetouring()
+        IncludeScanning()
         IncludeSDKTier0()
         IncludeSDKTier1()
         --IncludeSDKTier3()
@@ -31,8 +25,6 @@ CreateWorkspace({name = "template", abi_compatible = false})
         --IncludeSDKBitmap()
         --IncludeSDKVTF()
         --IncludeSteamAPI()
-        --IncludeDetouring()
-        --IncludeScanning()
 
         filter("system:windows")
             files({"source/win32/*.cpp", "source/win32/*.hpp"})
